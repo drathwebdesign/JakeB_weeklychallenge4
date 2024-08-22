ae4fbd03983ec4e01cf91f9ae52e43f654a3e2c8
Jake Bignell <jake.bignell@netmatters-scs.com>
2024-08-22 17:34:51 +0100

    WeeklyChallenge4
    WeeklyChallenge4 bugs fixed

---
+++ JakeB_weeklychallenge4/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+ {
+    Vector3 startPosition;
+    public float moveSpeed = 2f;
+    float bgWidth;
+
+    void Start() {
+        startPosition = transform.position;
+        bgWidth = GetComponent<BoxCollider>().size.x;
+    }
+
+
+    void Update() {
+        if (transform.position.x < startPosition.x - (bgWidth / 2)) {
+            transform.position = startPosition;
+        }
+
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    }
+}
